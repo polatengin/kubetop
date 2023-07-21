80feb6a996ab182a0bc79a5acc709c023885c170
polatengin <enpolat@microsoft.com>
2023-07-21 20:00:47 +0000

    creating initial project

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+}
