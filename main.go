7e05e39fecf554cd11ab79350cad0ce9b454b20e
polatengin <enpolat@microsoft.com>
2023-07-21 20:01:29 +0000

    adding function that returns the existing kubernetes cluster config

--- main.go
+++ main.go
@@ -17,3 +17,18 @@ import (
 
 func main() {
 }
+
+func loadKubeConfig() (*rest.Config, error) {
+	home := homedir.HomeDir()
+	kubeconfig := filepath.Join(home, ".kube", "config")
+
+	// Use in-cluster config if it exists, otherwise, use the kubeconfig from the local computer
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return config, nil
+}
